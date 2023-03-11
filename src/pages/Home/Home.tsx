ae9f68788679e56a3c758ea2c3312fb332dd3ef6
yamato-nagai <umarovalisher132@gmail.com>
2023-03-11 21:09:48 +0500

    create files

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+import Header from '../../components/Header/Header'
+
+const Home:FC = () => {
+  return (
+    <div>
+      Home
+      <Header />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export interface types {
+    name: string
+}
